9aea127d2d4193eeb6b31e2c095d3b15ec468c7a
Adarsh  Raj Tiwari <myselfadarsh1463@gmail.com>
2025-11-19 02:07:00 +0530

    Added Conditionals.java program file

---
+++ Conditionals.java
@@ -0,0 +1,17 @@
+public class Conditionals {
+    public static void main(String[] args)
+    {
+        int x = 5;
+        int y = 7;
+        int z = 9;
+
+        if(x > y && x > z)
+            System.out.println("X is Greatest");
+        else if(y > x && y > z)
+            System.out.println("Y is Greatest");
+        else 
+            System.out.println("Z is Greatest");
+
+
+    }
+}
