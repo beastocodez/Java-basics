5430febcd08d0dbfedadb940e0bcad7d63bebd24
Adarsh  Raj Tiwari <myselfadarsh1463@gmail.com>
2025-12-21 22:51:02 +0530

    Added For Loop Examples in Loops folder

---
+++ Loops/ForLoop.java
@@ -0,0 +1,15 @@
+public class ForLoop{
+    public static void main(String[] args){
+        for(int i=1; i<=5; i++)
+            {
+                System.out.println("DAY " + i);
+                
+                for(int j=1; j<=9;j++)
+                {
+                    System.out.println("     Hour " + (j+8));
+
+                }
+
+            }
+    }
+}
